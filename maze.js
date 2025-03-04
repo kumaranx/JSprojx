2c29fad2aa633e9f63ad09d7003698ead301fe97
Kumaran <kumaran.0q@gmail.com>
2025-03-04 14:08:49 +0530

    updated user input prompts
    User Inputs

--- maze.js
+++ maze.js
@@ -1,3 +1,17 @@
 const prompt = require('prompt-sync')()
 const name = prompt('Yoo, tell me ur name.. ')
-console.log(`Welcome to the game, Mr ${name}.`)
+console.log(`Welcome to the game, ${name}.`)
+
+const shouldWePlay = prompt('Which pill do u choose ')
+
+// const condition = shouldWePlay.toLowerCase() === 'red'
+// console.log(condition)
+
+if (shouldWePlay.toLowerCase() === 'red') {
+  // Game Logic
+
+} else if (shouldWePlay.toLowerCase() === 'blue') {
+  console.log(`your loss ${name}`)
+} else {
+  console.log('Fix ur typo man')
+}
